b8ec00897ee9b25cf2512edc251957c74be52f7e
tbrzc <javavillabon@soy.sena.edu.co>
2024-03-07 08:15:58 -0500

    basic login

--- FIFAclone/FIFAclone/views.py
+++ FIFAclone/FIFAclone/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 def index(request):
     return render(request, 'index.html',{
+
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('admin:index')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+
+    return render(request, 'login.html',{
 
     })
