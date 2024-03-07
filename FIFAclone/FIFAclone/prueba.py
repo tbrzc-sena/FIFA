ed194e342ea50ba89584806e8fea6cc6ec9a34c0
tbrzc <javavillabon@soy.sena.edu.co>
2024-03-06 19:20:42 -0500

    env script

--- FIFAclone/FIFAclone/prueba.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-NODE_MODULES_DIR = Path(__file__).resolve().parent.parent.parent
-print(NODE_MODULES_DIR)
---
+++ configure_env.py
@@ -0,0 +1,20 @@
+import subprocess
+
+comandos = [
+    "python -m venv env",
+    ".\\env\\Scripts\\activate",
+    "python -m pip install -r FIFAclone/requirements.txt",
+    "npm install",
+    "python manage.py collectstatic"
+]
+
+for comando in comandos:
+    resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
+
+    if resultado.returncode == 0:
+        print("La ejecución del comando fue exitosa.")
+        print("Salida del comando:")
+        print(resultado.stdout)
+    else:
+        print("Hubo un error al ejecutar el comando.")
+        print("Error:", resultado.stderr)
